f658802220b3338015cc4163c201ecd7d7e5d36c
Christoff <20072015@sun.ac.za>
2020-11-24 13:09:16 +0200

    hspec added

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import ReDfa
+import Test.Hspec
+import Test.QuickCheck
+import ReDfa (Regex)
+import Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
+
+prop_indemp_simplify :: Regex -> Bool
+prop_indemp_simplify r = (simplify (simplify r)) == simplify r
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "ReDfa.simplify" $ do
+    modifyMaxSuccess (const 1000) $ it "is indempotent" $ property $
+      prop_indemp_simplify
+
+    {-it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      evaluate (head []) `shouldThrow` anyException-}
